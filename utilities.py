a5bfb8b1e47063c70c310b1322b7a4e71b4ff8ab
Satchel Baldwin <satchelbaldwin@bennington.edu>
2019-03-11 01:51:16 -0400

    ugh

--- utilities.py
+++ utilities.py
@@ -110,6 +110,21 @@ def condition_v(word, suffix):
             return True
     return False
 
+def condition_d(word):
+    if word[-1] == word[-2]:
+        return (True, word[-1])
+    else:
+        return (False, '')
+
+def condition_o(word):
+    if len(word) < 3:
+        return False
+    s = word[-3:]
+    if consonant(s[0]) and not consonant(s[1]) and consonant(s[2]):
+        if s[2] != 'w' and s[2] != 'x' and s[2] != 'y':
+            return True
+    return False
+
 def step_1a(word):
     d = {
             "sses": "ss"
@@ -119,12 +134,16 @@ def step_1a(word):
         }
     word = r_suffix_d(word, d)
     return word
+
 def step_1b(word):
     m = measure(word)
-    (word, m) = r_suffix(word, "eed", "ee") if m > 0 else word
-    if m:
-        break
-    if (condition_v(word) and (word.endswith("ed") or word.endswith("ing"))):
+    (word, changed) = r_suffix(word, "eed", "ee") if m > 0 else (word, False)
+    if changed:
+        return word
+    if (
+            (condition_v(word, 'ed') or condition_v(word, 'ing')) 
+            and (word.endswith("ed") or word.endswith("ing"))
+        ):
         d = {
                 "ed" : ""
             ,   "ing": ""
@@ -135,27 +154,107 @@ def step_1b(word):
             ,   "bl": "ble"
             ,   "iz": "ize"
             }
-
-
+        new_word = r_suffix_d(word, d)
+        if (word == new_word):
+            (r, v) = condition_d(word)
+            if r and not (v == 'l' or v =='s' or v == 'z'):
+                word = word[:-1]
+            elif m == 1 and condition_o(word):
+                word = word + 'e'
+    return word
 
 def step_1c(word):
-    pass
+    if condition_v(word, 'i') and word.endswith('y'):
+        (word, changed) = r_suffix(word, 'y', 'i')
+    return word
+
 def step_2(word):
-    pass
+    m = measure(word)
+    d = {
+            "ational": "ate",
+            "tional": "tion",
+            "enci": "ence",
+            "anci": "ance",
+            "izer": "ize",
+            "abli": "able",
+            "alli": "al",
+            "entli": "ent",
+            "eli": "e",
+            "ousli": "ous",
+            "ization": "ize",
+            "ation": "ate",
+            "ator": "ate",
+            "alism": "al",
+            "iveness": "ive",
+            "fulness": "ful",
+            "ousness": "ous",
+            "aliti": "al",
+            "iviti": "ive",
+            "biliti": "ble"
+        }
+    if m > 0:
+        word = r_suffix_d(word, d)
+    return word
+
 def step_3(word):
-    pass
+    m = measure(word)
+    d = {
+            "icate": "ic",
+            "ative": "",
+            "alize": "al",
+            "iciti": "ic",
+            "ical": "ic",
+            "ful": "",
+            "ness": ""
+        }
+    if m > 0:
+        word = r_suffix_d(word, d)
+    return word
+
 def step_4(word):
-    pass
+    m = measure(word)
+    def r_suffix_blank(word, l):
+        d = {}
+        for s in l:
+            d[s] = ''
+        return r_suffix_d(word, d)
+    l = ["al", "ance", "ence", "er", "ic", "able", "ible", "ant", "ement", 
+            "ment", "ent", "ou", "ism", "ate", "iti", "ous", "ive", "ize"]
+    if m > 1:
+        if word.endswith("tion") or word.endswith("sion"):
+            (word, changed) = r_suffix(word, 'tion', '')
+            (word, changed) = r_suffix(word, 'sion', '')
+            return word
+        word = r_suffix_blank(word, l)
+    return word
+
 def step_5a(word):
-    pass
+    m = measure(word)
+    if m > 1:
+        (word, changed) = r_suffix(word, "e", "")
+    if m == 0 and not condition_o(word):
+        (word, changed) = r_suffix(word, "e", "")
+    return word
+
 def step_5b(word):
-    pass
+    m = measure(word)
+    if m > 1 and condition_d(word) and word.endswith("l"):
+        (word, changed) = r_suffix(word, "ll", "l")
+    return word
 
 def porter_stemmer(w):
     words = []
     for word in w:
         word = word.lower()
-        if w[-1] != '-':
-            words.append(word)
-    # stemming steps
-    
+        if word[-1] != '-' and len(word) > 1:
+            w1a = step_1a(word)
+            w1b = step_1b(w1a)
+            w1c = step_1c(w1b)
+            w2 = step_2(w1c)
+            w3 = step_3(w2)
+            w4 = step_4(w3)
+            w5a = step_5a(w4)
+            w5b = step_5b(w5a)
+            words.append(w5b)
+    return words
+
